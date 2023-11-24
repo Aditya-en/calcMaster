fb46ac588c93e2565b6be8af7ec05fa4eec6d868
kharshit801 <harshit.20238008@mnnit.ac.in>
2023-11-25 03:55:35 +0530

    switch app functioning

--- app/src/main/java/com/example/calc/MainActivity.java
+++ app/src/main/java/com/example/calc/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.calc;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
